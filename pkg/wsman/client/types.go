ee6cd819eb09b65fb30e8f34d6a9266d2842eb78
Mike <mjohanson@gesdate.com>
2024-10-15 09:47:04 -0700

    thoughts

--- pkg/wsman/client/types.go
+++ pkg/wsman/client/types.go
@@ -1,6 +1,9 @@
 package client
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+)
 
 // Parameters struct defines the connection settings for wsman client.
 type Parameters struct {
@@ -14,4 +17,5 @@ type Parameters struct {
 	Transport         http.RoundTripper
 	IsRedirection     bool
 	PinnedCert        string
+	Connection        net.Conn
 }
